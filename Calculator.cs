3f892ea90cf294b07f74c37ee00a46a07c741eb1
SHINXB1 <148068951+SHINXB1@users.noreply.github.com>
2025-04-23 15:24:18 +0300

    Add files via upload
    initial

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculatorProject
+{
+    public static class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorProject
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_ReturnsCorrectResult()
+        {
+            double result = Calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Subtract_ReturnsCorrectResult()
+        {
+            double result = Calculator.Subtract(10, 4);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Add_WithNegativeNumbers_WorksCorrectly()
+        {
+            double result = Calculator.Add(-2, -3);
+            Assert.AreEqual(-5, result);
+        }
+
+        [TestMethod]
+        public void Subtract_WithNegativeNumbers_WorksCorrectly()
+        {
+            double result = Calculator.Subtract(-10, -5);
+            Assert.AreEqual(-5, result);
+        }
+    }
+}
